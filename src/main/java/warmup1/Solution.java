cc6fe1b02758f9ccb6f3a4553e08a718c437983e
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2018-12-03 10:36:11 +0200

    solved frontBack (warmup1)

--- src/main/java/warmup1/Solution.java
+++ src/main/java/warmup1/Solution.java
@@ -62,4 +62,14 @@ public class Solution {
         return str.replaceFirst(String.valueOf(str.charAt(n)), "");
     }
 
+    public String frontBack(String str) {
+        if(str.length() < 2){
+            return str;
+        }else{
+            char first = str.charAt(0);
+            char last = str.charAt(str.length()-1);
+            str = str.substring(1, str.length()-1);
+            return last+str+first;
+        }
+    }
 }
