674a8be272e8af4739fe256e688946eeee6f6c46
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2018-12-18 12:15:18 +0200

    solved EndUP (Warmup1)

--- src/main/java/warmup1/Solution.java
+++ src/main/java/warmup1/Solution.java
@@ -203,4 +203,11 @@ public class Solution {
     public boolean lastDigit(int a, int b) {
         return a % 10 == b % 10;
     }
+
+    public String endUp(String str) {
+        if (str.length() < 4) {
+            return str.toUpperCase();
+        }
+        return str.substring(0, str.length() - 3) + str.substring(str.length() - 3).toUpperCase();
+    }
 }
