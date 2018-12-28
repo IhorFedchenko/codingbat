a5a184f08c0f6de77cdba2ca0031897a77e1284b
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2018-12-28 11:37:22 +0200

    solved NoTriples (Warmup2)

--- src/main/java/warmup2/Solution.java
+++ src/main/java/warmup2/Solution.java
@@ -169,4 +169,13 @@ public class Solution {
         }
         return result;
     }
+
+    public boolean noTriples(int[] nums) {
+        for (int i = 0; i < nums.length - 2 ; i++) {
+            if (nums[i] ==  nums[i+1] && nums[i+1] == nums[i+2]){
+                return false;
+            }
+        }
+        return true;
+    }
 }
