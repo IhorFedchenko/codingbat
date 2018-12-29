261329c835325fdd74a8ca2d47e509c7ac6517de
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2018-12-29 15:32:45 +0200

    solved HelloName (string1)

--- src/main/java/warmup2/Solution.java
+++ src/main/java/warmup2/Solution.java
@@ -162,8 +162,8 @@ public class Solution {
 
     public int array667(int[] nums) {
         int result = 0;
-        for (int i = 0; i < nums.length -1 ; i++) {
-            if (nums[i] == 6 && (nums[i+1] == 6)|| nums[i+1] ==7){
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 6 && (nums[i + 1] == 6) || nums[i + 1] == 7) {
                 result++;
             }
         }
---
+++ src/test/java/string1/HelloName.java
@@ -0,0 +1,68 @@
+package string1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HelloName {
+
+    @Test
+    public void nello_name_Bob(){
+        Solution solution = new Solution();
+        assertEquals("Hello Bob!", solution.helloName("Bob"));
+    }
+
+    @Test
+    public void nello_name_Alice(){
+        Solution solution = new Solution();
+        assertEquals("Hello Alice!", solution.helloName("Alice"));
+    }
+
+    @Test
+    public void nello_name_X(){
+        Solution solution = new Solution();
+        assertEquals("Hello X!", solution.helloName("X"));
+    }
+
+    @Test
+    public void nello_name_Dolly(){
+        Solution solution = new Solution();
+        assertEquals("Hello Dolly!", solution.helloName("Dolly"));
+    }
+
+    @Test
+    public void nello_name_Alpha(){
+        Solution solution = new Solution();
+        assertEquals("Hello Alpha!", solution.helloName("Alpha"));
+    }
+
+    @Test
+    public void nello_name_Omega(){
+        Solution solution = new Solution();
+        assertEquals("Hello Omega!", solution.helloName("Omega"));
+    }
+
+    @Test
+    public void nello_name_Goodbye(){
+        Solution solution = new Solution();
+        assertEquals("Hello Goodbye!", solution.helloName("Goodbye"));
+    }
+
+    @Test
+    public void nello_name_ho_ho_ho(){
+        Solution solution = new Solution();
+        assertEquals("Hello ho ho ho!", solution.helloName("ho ho ho"));
+    }
+
+    @Test
+    public void nello_name_xyz(){
+        Solution solution = new Solution();
+        assertEquals("Hello xyz!", solution.helloName("xyz"));
+    }
+
+    @Test
+    public void nello_name_Hello(){
+        Solution solution = new Solution();
+        assertEquals("Hello Hello!", solution.helloName("Hello"));
+    }
+}
