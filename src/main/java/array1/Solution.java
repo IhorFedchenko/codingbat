e1ccc2d959163785acb6eab1d985e3157c862afd
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2019-01-17 11:41:36 +0200

    solved Sum3 (Array-1)

--- src/main/java/array1/Solution.java
+++ src/main/java/array1/Solution.java
@@ -22,4 +22,8 @@ public class Solution {
         return a[0] == b[0] || a[a.length-1] == b[b.length-1];
     }
 
+    public int sum3(int[] nums) {
+        return nums[0] + nums[1] + nums[2];
+    }
+
 }
