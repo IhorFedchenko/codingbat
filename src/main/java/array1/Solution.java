c1bbd64fd9a881c026fa878391bc11e1088e87ef
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2019-01-20 18:23:08 +0200

    solved MaxEnd3 (array1)

--- src/main/java/array1/Solution.java
+++ src/main/java/array1/Solution.java
@@ -41,4 +41,15 @@ public class Solution {
         result[2] = nums[0];
         return result;
     }
+
+    public int[] maxEnd3(int[] nums) {
+        if (nums[0] > nums[2]){
+            return new int[]{nums[0], nums[0], nums[0]};
+        }
+
+        if (nums[2] > nums[0]){
+            return new int[]{nums[2], nums[2], nums[2]};
+        }
+        return new int[]{nums[0], nums[0], nums[0]};
+    }
 }
