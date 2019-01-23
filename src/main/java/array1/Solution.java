a2517376b34baa62c7a09a6d48742d56dba6e30c
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2019-01-23 16:52:40 +0200

    solved No23 (Array-1)

--- src/main/java/array1/Solution.java
+++ src/main/java/array1/Solution.java
@@ -74,4 +74,8 @@ public class Solution {
     public boolean has23(int[] nums) {
         return( nums[0]== 2 || nums[1] == 2 ) || ( nums[0] == 3 || nums[1] == 3);
     }
+
+    public boolean no23(int[] nums) {
+        return !(nums[0]== 2 || nums[0] == 3 || nums[1]== 2 || nums[1] == 3);
+    }
 }
