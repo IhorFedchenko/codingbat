2670ac0d04b176b94cee66a8c153240b3a727616
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2019-01-28 11:19:38 +0200

    solved MidThree (Array-1)

--- src/main/java/array1/Solution.java
+++ src/main/java/array1/Solution.java
@@ -137,4 +137,12 @@ public class Solution {
         nums[nums.length-1] = tmp;
         return nums;
     }
+
+    public int[] midThree(int[] nums) {
+        int[] result = new int[3];
+        result[0] = nums[nums.length/2-1];
+        result[1] = nums[nums.length/2];
+        result[2] = nums[nums.length/2+1];
+        return result;
+    }
 }
---
+++ src/test/java/array1/MidThree.java
@@ -0,0 +1,34 @@
+package array1;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+
+public class MidThree {
+
+    @Test
+    public void mid_three_v01(){
+        Solution solution = new Solution();
+        int[] input = {1, 2, 3, 4, 5};
+        int[] expected = {2, 3, 4};
+        assertEquals(Arrays.toString(expected), Arrays.toString(solution.midThree(input)));
+    }
+
+    @Test
+    public void mid_three_v02(){
+        Solution solution = new Solution();
+        int[] input = {8, 6, 7, 5, 3, 0, 9};
+        int[] expected = {7, 5, 3};
+        assertEquals(Arrays.toString(expected), Arrays.toString(solution.midThree(input)));
+    }
+
+    @Test
+    public void mid_three_v03(){
+        Solution solution = new Solution();
+        int[] input = {1, 2, 3};
+        int[] expected = {1, 2, 3};
+        assertEquals(Arrays.toString(expected), Arrays.toString(solution.midThree(input)));
+    }
+}
