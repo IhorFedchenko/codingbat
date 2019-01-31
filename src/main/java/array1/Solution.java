c95f16fda72ee498ef0c565231f5ac344a1caadf
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2019-01-31 14:45:51 +0200

    solved Unlucky1 (Array-1)

--- src/main/java/array1/Solution.java
+++ src/main/java/array1/Solution.java
@@ -163,4 +163,16 @@ public class Solution {
         }
         return nums;
     }
+
+    public boolean unlucky1(int[] nums) {
+        if (nums.length < 2) {
+            return false;
+        }
+        if (nums.length == 2 && nums[0] == 1 && nums[1] == 3) {
+            return true;
+        }
+        return nums.length > 2 && nums[0] == 1 && nums[1] == 3 ||
+                nums.length > 2 && nums[1] == 1 && nums[2] == 3 ||
+                nums.length > 2 && nums[nums.length - 2] == 1 && nums[nums.length - 1] == 3;
+    }
 }
