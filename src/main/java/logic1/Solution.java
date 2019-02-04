76730ff5b495ffa93e72d243c9d6a3778adae241
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2019-02-04 21:15:29 +0200

    solved SquirrelPlay (array1)

--- src/main/java/logic1/Solution.java
+++ src/main/java/logic1/Solution.java
@@ -21,4 +21,11 @@ public class Solution {
         }
         return 1;
     }
+
+    public boolean squirrelPlay(int temp, boolean isSummer) {
+        if (isSummer){
+            return temp >= 60 && temp <= 100;
+        }
+        return  temp >= 60 && temp <=90;
+    }
 }
---
+++ src/test/java/logic1/SquirrelPlay.java
@@ -0,0 +1,88 @@
+package logic1;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+
+public class SquirrelPlay {
+
+    @Test
+    public void squirrelPlay_v01(){
+        Solution solution = new Solution();
+        assertTrue(solution.squirrelPlay(70, false));
+    }
+
+    @Test
+    public void squirrelPlay_v02(){
+        Solution solution = new Solution();
+        assertFalse(solution.squirrelPlay(95, false));
+    }
+
+    @Test
+    public void squirrelPlay_v03(){
+        Solution solution = new Solution();
+        assertTrue(solution.squirrelPlay(95, true));
+    }
+
+    @Test
+    public void squirrelPlay_v04(){
+        Solution solution = new Solution();
+        assertTrue(solution.squirrelPlay(90, false));
+    }
+
+    @Test
+    public void squirrelPlay_v05(){
+        Solution solution = new Solution();
+        assertTrue(solution.squirrelPlay(90, true));
+    }
+
+    @Test
+    public void squirrelPlay_v06(){
+        Solution solution = new Solution();
+        assertFalse(solution.squirrelPlay(50, false));
+    }
+
+    @Test
+    public void squirrelPlay_v07(){
+        Solution solution = new Solution();
+        assertFalse(solution.squirrelPlay(50, true));
+    }
+
+    @Test
+    public void squirrelPlay_v08(){
+        Solution solution = new Solution();
+        assertFalse(solution.squirrelPlay(100, false));
+    }
+
+    @Test
+    public void squirrelPlay_v09(){
+        Solution solution = new Solution();
+        assertTrue(solution.squirrelPlay(100, true));
+    }
+
+    @Test
+    public void squirrelPlay_v10(){
+        Solution solution = new Solution();
+        assertFalse(solution.squirrelPlay(105, true));
+    }
+
+    @Test
+    public void squirrelPlay_v11(){
+        Solution solution = new Solution();
+        assertFalse(solution.squirrelPlay(59, false));
+    }
+
+    @Test
+    public void squirrelPlay_v12(){
+        Solution solution = new Solution();
+        assertFalse(solution.squirrelPlay(59, true));
+    }
+
+    @Test
+    public void squirrelPlay_v13(){
+        Solution solution = new Solution();
+        assertTrue(solution.squirrelPlay(60, false));
+    }
+
+}
