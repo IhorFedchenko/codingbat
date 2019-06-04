5eb11238f542d0b46e88ad9fa8496c12e8771cae
Ihor Fedchenko <fedchenko.ihor@gmail.com>
2019-06-04 14:08:43 +0300

    solved sameStarChar (String-2)

--- src/main/java/string2/Solution.java
+++ src/main/java/string2/Solution.java
@@ -151,5 +151,15 @@ public class Solution {
         }
         return str.substring(first + 5, last);
     }
-}
 
+    public boolean sameStarChar(String str) {
+        for (int i = 1; i < str.length() - 1; i++) {
+            if (str.charAt(i) == '*') {
+                if (str.charAt(i - 1) != str.charAt(i + 1)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
